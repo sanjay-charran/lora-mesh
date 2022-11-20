f40968d153d419064a4f8fc9e6bdbb3e42c67f95
sanjay-charran <sanjaycharran@gmail.com>
2022-11-20 17:52:25 -0400

    Created new module with default files

---
+++ helper/lora-mesh-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef LORA_MESH_HELPER_H
+#define LORA_MESH_HELPER_H
+
+#include "ns3/lora-mesh.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* LORA_MESH_HELPER_H */
+
---
+++ model/lora-mesh.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef LORA_MESH_H
+#define LORA_MESH_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* LORA_MESH_H */
+
