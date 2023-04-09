62b9394b3bc01f07d686ee5a83e92bb6c882a90c
sanjay-charran <sanjaycharran@gmail.com>
2023-04-09 17:55:44 -0400

    Added in GPLv2 license for source files

--- model/lora-mesh.h
+++ model/lora-mesh.h
@@ -1,6 +1,26 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2023 Sanjay Charran
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Sanjay Charran <sanjaycharran@gmail.com>
+ */
 #ifndef LORA_MESH_H
 #define LORA_MESH_H
+
+/*  Header used for convenience of using module */
 
 #include "ns3/ascii-helper-for-lora.h"
 #include "ns3/lora-phy.h"
